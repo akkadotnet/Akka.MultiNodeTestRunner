506ce55cadb966730016757578c131820e3b9593
IgorFedchenko <igor@fedchenko.pro>
2020-02-01 09:59:10 +0300

    Added sample spec

---
+++ src/Akka.MultiNodeTestRunner.SampleTests/EmptySpec.cs
@@ -0,0 +1,43 @@
+using Akka.Cluster.TestKit;
+using Akka.Remote.TestKit;
+
+namespace Akka.MultiNodeTestRunner.SampleTests
+{
+    public class EmptySpecConfig : MultiNodeConfig
+    {
+        public RoleName First { get; }
+        public RoleName Second { get; }
+
+        public EmptySpecConfig()
+        {
+            First = Role("first");
+            Second = Role("second");
+        }
+    }
+
+    public class EmptySpec : MultiNodeClusterSpec
+    {
+        readonly EmptySpecConfig _config;
+
+        public EmptySpec() : this(new EmptySpecConfig())
+        {
+        }
+
+        private EmptySpec(EmptySpecConfig config) : base(config, typeof(EmptySpec))
+        {
+            _config = config;
+        }
+
+        [MultiNodeFact]
+        public void Should_start_and_join_cluster()
+        {
+            RunOn(StartClusterNode, _config.First);
+
+            EnterBarrier("first-started");
+
+            RunOn(() => Cluster.Join(GetAddress(_config.First)), _config.Second);
+            
+            EnterBarrier("after");
+        }
+    }
+}
